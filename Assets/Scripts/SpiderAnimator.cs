5112a3f5a9328ecf2ceda285b3d9a7672177f2c6
Matt Scorca <mscorca@ucsc.edu>
2015-05-16 09:47:36 -0700

    Initial project commit

---
+++ Assets/Scripts/SpiderAnimator.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpiderAnimator : MonoBehaviour {
+
+    public Animation anim;
+    
+    void Start() {
+        anim = GetComponent<Animation>();
+        
+        anim.Play("Death");
+    }
+}
